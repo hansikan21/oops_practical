07fcb5f94d41aa7b9503d779e4fa3bd0e9daf088
hansikan21 <2420030420@klh.edu.in>
2025-07-26 09:19:12 +0530

    Add files via upload

---
+++ prac1.java
@@ -0,0 +1,14 @@
+package oops_practical;
+
+public class prac1 {
+	public static void main(String[] args) {
+		int a = 5, b = 7;
+		System.out.println("Addition: " + (a + b));
+		System.out.println("Subtraction: " + (a - b));
+		System.out.println("Multiplication: " + (a * b));
+		System.out.println("Division: " + (a / b));
+		System.out.println("Modulus: " + (a % b));
+		
+	}
+
+}
